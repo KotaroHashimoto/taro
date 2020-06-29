86de2f7ccc7a563153ec69ce0ba21eebb6f336e8
Kotaro Hashimoto <hasimoto.kotaro@gmail.com>
2020-06-30 00:47:38 +0900

    add draw chart
    Signed-off-by: Kotaro Hashimoto <hasimoto.kotaro@gmail.com>

--- TaroLogger.py
+++ TaroLogger.py
@@ -11,6 +11,8 @@ import numpy as np
 import os
 from glob import glob
 from collections import OrderedDict
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 MEDIADIR = '../usb/media/motion/'
 LOGDIR = '../usb/logs/'
@@ -18,7 +20,7 @@ INTERVAL = 60 #[s]
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
-col = ['date', 'time', 'activity', 'temp[*C]', 'humid[%}', 'CPU[*C]', 'CPU[v]', 'CPU[MHz]', 'CPU[%]', 'memory[%]', 'rootfs[%]', 'usb[%]']
+col = ['date', 'time', 'activity', 'temp[*C]', 'humid[%]', 'CPU[*C]', 'CPU[v]', 'CPU[MHz]', 'CPU[%]', 'memory[%]', 'rootfs[%]', 'usb[%]']
 series = [pd.Series([np.nan for x in col], index = col) \
           for i in range(round(24 * 60 * 60 / INTERVAL))]
 currentIndex = 0
@@ -50,6 +52,30 @@ def countMotion(dtFrom, dtTo):
             num += 1
 
     return num, oldest
+
+
+def drawChart(df):
+
+    hm = [int(x[:4]) for x in df['time']]
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ln1 = ax1.plot(hm, df['temp[*C]'], 'C0',label = 'temp[*C]')
+
+    ax2 = ax1.twinx()
+    ln2 = ax2.plot(hm, df['humid[%]'], 'C1', label = 'humid[%]')
+
+    h1, l1 = ax1.get_legend_handles_labels()
+    h2, l2 = ax2.get_legend_handles_labels()
+    ax1.legend(h1 + h2, l1 + l2, loc = 'lower right')
+
+    ax1.set_xlabel('time')
+    ax1.set_ylabel('temp[*C]')
+    ax1.grid(True)
+    ax2.set_ylabel('humid[%]')
+
+    plt.savefig('hoge.png')
+
     
 
 def watch(signum, frame):
@@ -68,7 +94,7 @@ def watch(signum, frame):
     day, tm = currentDateTime.strftime('%Y%m%d-%H%M%S').split('-')
 
     series[currentIndex][['date', 'time']] = [day, tm]
-    series[currentIndex][['humid[%}', 'temp[*C]']] = [round(x, 2) if x else 0 for x in Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)]
+    series[currentIndex][['humid[%]', 'temp[*C]']] = [round(x, 2) if x else 0 for x in Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)]
     series[currentIndex][['CPU[*C]', 'CPU[v]', 'CPU[MHz]']] = [vcmd.measure_temp(), vcmd.measure_volts('core'), round(vcmd.measure_clock('arm') / 1000000)]
     series[currentIndex][['CPU[%]', 'memory[%]', 'rootfs[%]', 'usb[%]']] = [psutil.cpu_percent(interval = 1), psutil.virtual_memory().percent, psutil.disk_usage('/').percent, psutil.disk_usage('/home/pi/usb').percent]
     series[currentIndex]['activity'], oldestJPG = countMotion(previousDateTime, currentDateTime)
@@ -86,7 +112,8 @@ def watch(signum, frame):
                        [x for x in series if not pd.isnull(x['date'])] \
                        if y['date'] == day]) \
            .sort_values(by = 'time', ascending = False)
-    
+
+    drawChart(df)
     df.to_csv(LOGDIR + day + '.csv', encoding = 'utf-8', index = False)
     print(day + '.csv dumped with', len(df), 'records (' + tm[:2] + ':' + tm[2:4] + ':' + tm[4:] + ')')
     print(series[currentIndex])
