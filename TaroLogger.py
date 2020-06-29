65c5884eabeff411b52e5d1ea469cd1f00e4cf58
Kotaro Hashimoto <hasimoto.kotaro@gmail.com>
2020-06-29 20:19:17 +0900

    update taro logger
    Signed-off-by: Kotaro Hashimoto <hasimoto.kotaro@gmail.com>

--- TaroLogger.py
+++ TaroLogger.py
@@ -10,18 +10,47 @@ import pandas as pd
 import numpy as np
 import os
 from glob import glob
+from collections import OrderedDict
 
+MEDIADIR = '../usb/media/motion/'
 LOGDIR = '../usb/logs/'
 INTERVAL = 60 #[s]
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
-col = ['date', 'time', 'temp[*C]', 'humid[%}', 'CPU[*C]', 'CPU[v]', 'CPU[MHz]', 'CPU[%]', 'memory[%]', 'rootfs[%]', 'usb[%]']
+col = ['date', 'time', 'activity', 'temp[*C]', 'humid[%}', 'CPU[*C]', 'CPU[v]', 'CPU[MHz]', 'CPU[%]', 'memory[%]', 'rootfs[%]', 'usb[%]']
 series = [pd.Series([np.nan for x in col], index = col) \
           for i in range(round(24 * 60 * 60 / INTERVAL))]
 currentIndex = 0
+previousDateTime = None
 vcmd = vcgencmd.Vcgencmd()
 
+
+def countMotion(dtFrom, dtTo):
+
+    if not dtFrom:
+        return 0, None
+
+    global MEDIADIR
+    
+    fname2time = OrderedDict()
+    oldest = None
+    num = 0
+
+    for f in glob(MEDIADIR + '*jpg'):
+        timeStamp = datetime.fromtimestamp(os.path.getmtime(f))
+        fname2time[f] = timeStamp
+
+        if not oldest:
+            oldest = f
+        elif timeStamp < fname2time[oldest]:
+            oldest = f
+
+        if dtFrom <= timeStamp < dtTo:
+            num += 1
+
+    return num, oldest
+    
 
 def watch(signum, frame):
 
@@ -33,14 +62,21 @@ def watch(signum, frame):
     global series
     global currentIndex
     global vcmd
+    global previousDateTime
 
-    day, tm = datetime.now().strftime('%Y%m%d-%H%M%S').split('-')
+    currentDateTime = datetime.now()
+    day, tm = currentDateTime.strftime('%Y%m%d-%H%M%S').split('-')
 
     series[currentIndex][['date', 'time']] = [day, tm]
     series[currentIndex][['humid[%}', 'temp[*C]']] = [round(x, 2) for x in Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)]
     series[currentIndex][['CPU[*C]', 'CPU[v]', 'CPU[MHz]']] = [vcmd.measure_temp(), vcmd.measure_volts('core'), round(vcmd.measure_clock('arm') / 1000000)]
     series[currentIndex][['CPU[%]', 'memory[%]', 'rootfs[%]', 'usb[%]']] = [psutil.cpu_percent(interval = 1), psutil.virtual_memory().percent, psutil.disk_usage('/').percent, psutil.disk_usage('/home/pi/usb').percent]
-
+    series[currentIndex]['activity'], oldestJPG = countMotion(previousDateTime, currentDateTime)
+    
+    if 90 < series[currentIndex]['usb[%]'] and oldestJPG:
+        os.remove(oldestJPG)
+        print(oldestJPG, 'deleted due to free space shortage.')
+        
     if 95 < series[currentIndex]['usb[%]']:
         fname = min(glob(LOGDIR + '20*.csv'))
         os.remove(fname)
@@ -56,10 +92,11 @@ def watch(signum, frame):
     print(series[currentIndex])
 
     currentIndex = (currentIndex + 1) % len(series)
+    previousDateTime = currentDateTime
 
 
 if __name__ == '__main__':
-    
+
     signal.signal(signal.SIGALRM, watch)
     signal.setitimer(signal.ITIMER_REAL, INTERVAL, INTERVAL)
     
